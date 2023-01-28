107666a48935950ee950b29180ae2a12854c3b4c
QRWells <qirui.wang@moegi.waseda.jp>
2023-01-28 16:32:16 +0900

    build: init commit

---
+++ router.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	handler "douyin_backend/biz/handler"
+
+	"github.com/cloudwego/hertz/pkg/app/server"
+)
+
+// customizeRegister registers customize routers.
+func customizedRegister(r *server.Hertz) {
+	r.GET("/ping", handler.Ping)
+
+	// your code ...
+}
---
+++ router_gen.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	router "douyin_backend/biz/router"
+
+	"github.com/cloudwego/hertz/pkg/app/server"
+)
+
+// register registers all routers.
+func register(r *server.Hertz) {
+
+	router.GeneratedRegister(r)
+
+	customizedRegister(r)
+}
