a58311b02bb5889501b576e6eb637285b0e7ba79
pengming <435788362@qq.com>
2023-02-05 23:00:52 +0800

    update user model

--- biz/model/user.go
+++ biz/model/user.go
@@ -1,7 +1,7 @@
 package model
 
-type User struct{
-	ID int64	`gorm:"primaryKey"`
-	Username string	`gorm:"index:idx_uname"`
-	Password string
+type User struct {
+	ID       int64  `gorm:"primaryKey"`
+	Username string `gorm:"size:32; unique; not null"`
+	Password string `gorm:"not null"`
 }
